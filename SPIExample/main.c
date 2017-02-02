788cc3957a1c6e41120513d6fe9dbc71400a68b6
Ty Winkler <TyWinkler@users.noreply.github.com>
2017-02-01 19:45:14 -0600

    Update main.c
    Edited some comments and headings

--- SPIExample/main.c
+++ SPIExample/main.c
@@ -1,56 +1,8 @@
 //*****************************************************************************
 //
-// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
-// 
-// 
-//  Redistribution and use in source and binary forms, with or without 
-//  modification, are permitted provided that the following conditions 
-//  are met:
-//
-//    Redistributions of source code must retain the above copyright 
-//    notice, this list of conditions and the following disclaimer.
-//
-//    Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the 
-//    documentation and/or other materials provided with the   
-//    distribution.
-//
-//    Neither the name of Texas Instruments Incorporated nor the names of
-//    its contributors may be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//*****************************************************************************
-
-//*****************************************************************************
-//
-// Application Name     - SPI Demo
-// Application Overview - The demo application focuses on showing the required 
-//                        initialization sequence to enable the CC3200 SPI 
-//                        module in full duplex 4-wire master and slave mode(s).
-// Application Details  -
-// http://processors.wiki.ti.com/index.php/CC32xx_SPI_Demo
-// or
-// docs\examples\CC32xx_SPI_Demo.pdf
-//
-//*****************************************************************************
-
-
-//*****************************************************************************
-//
-//! \addtogroup SPI_Demo
-//! @{
+// Application Name     - SPI Example for interfacing LPD8806 with CC3200 using
+//                        Code Composer Studio (CCS)
+// Authors              - Ty Winkler & Jonathan Ambrose
 //
 //*****************************************************************************
 
@@ -74,14 +26,10 @@
 #include "uart_if.h"
 #include "pinmux.h"
 
+// LPD8806 interface includes
 #include "LPD8806.h"
 
-
 #define APPLICATION_VERSION     "1.1.1"
-
-
-
-
 
 //*****************************************************************************
 //                 GLOBAL VARIABLES -- Start
@@ -109,6 +57,7 @@ extern uVectorEntry __vector_table;
 //*****************************************************************************
 //
 //! Board Initialization & Configuration
+//! This is TI's Initilization for the board
 //!
 //! \param  None
 //!
